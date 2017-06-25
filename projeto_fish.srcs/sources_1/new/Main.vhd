3fa5ff832c33b4faf455d876e20ce2b9504e70a0
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-06-25 12:05:53 -0300

    Tudo funfando, no coeficiente

--- projeto_fish.srcs/sources_1/new/Main.vhd
+++ projeto_fish.srcs/sources_1/new/Main.vhd
@@ -3,11 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Main is
     Port (  Enter : in STD_LOGIC;
+            Enter_Coef : in STD_LOGIC;
             memoria : in STD_LOGIC;
             modo : buffer STD_LOGIC;
             devivada: in STD_LOGIC;
             operacao : in STD_LOGIC_VECTOR(1 downto 0);
-            chave : in STD_LOGIC_VECTOR (4 downto 0));
+            chave : in STD_LOGIC_VECTOR (3 downto 0));
 end Main;
 
 architecture Behavioral of Main is
@@ -37,12 +38,25 @@ component Pol_Grau is
            Enter : in STD_LOGIC);
 end component;
 
-
+component Coeficiente is
+    Port ( Coef_MUX : in STD_LOGIC;
+           Chaves : in STD_LOGIC_VECTOR (3 downto 0);
+           Enter : in STD_LOGIC;
+           Grau : in STD_LOGIC_VECTOR (1 downto 0);
+           Coef_out_0 : out STD_LOGIC_VECTOR (3 downto 0) :="0000";
+           Coef_out_1 : out STD_LOGIC_VECTOR (3 downto 0):="0000";
+           Coef_out_2 : out STD_LOGIC_VECTOR (3 downto 0):="0000";
+           Coef_out_3 : out STD_LOGIC_VECTOR (3 downto 0):="0000";
+           Acabou : out STD_LOGIC);
+end component;
 
 ----------------- Signal ------------------------
 signal saidaEstado : STD_LOGIC_VECTOR(2 downto 0);
 signal saidaMUX : STD_LOGIC_VECTOR(6 downto 0);
 signal grauPol_1, grauPol_2: STD_LOGIC_VECTOR (1 downto 0);
+signal Coef_1_0, Coef_1_1,Coef_1_2,Coef_1_3: STD_LOGIC_VECTOR (3 downto 0) :="0000";
+signal Coef_2_0, Coef_2_1,Coef_2_2,Coef_2_3: STD_LOGIC_VECTOR (3 downto 0) :="0000";
+signal Corf_end_1,Corf_end_2 : STD_LOGIC;
 
 begin
 G0: MaquinaEstados port map (Enter=>enter,modo=>modo,saida=>saidaEstado);
@@ -50,5 +64,6 @@ G1: MUX3x7 port map (ProcessoSaida=>saidaMUX,Processo=>saidaEstado);
 G2: E0_Modo port map (MODO_MUX => saidaMUX(0), ENTER => Enter, MODO_CHAVE => Chave(0), MODO_OUT => Modo);
 G3: Pol_Grau port map (Grau_MUX => saidaMUX(1), Grau_Chave(0) => chave(0), Grau_Chave(1) => chave(1), Grau_out=>grauPol_1, Enter=>Enter);
 G4: Pol_Grau port map (Grau_MUX => saidaMUX(2), Grau_Chave(0) => chave(0), Grau_Chave(1) => chave(1), Grau_out=>grauPol_2, Enter=>Enter);
-
+G5: Coeficiente port map (Coef_MUX => saidaMUX(2), Chaves => chave, Enter => Enter_Coef, Grau => grauPol_1,Coef_out_0=>Coef_1_0 ,Coef_out_1=>Coef_1_1 ,Coef_out_2=>Coef_1_2, Coef_out_3=>Coef_1_3, Acabou=>Corf_end_1);
+G6: Coeficiente port map (Coef_MUX => saidaMUX(5), Chaves => chave, Enter => Enter_Coef, Grau => grauPol_2,Coef_out_0=>Coef_2_0 ,Coef_out_1=>Coef_2_1 ,Coef_out_2=>Coef_2_2, Coef_out_3=>Coef_2_3, Acabou=>Corf_end_2); 
 end Behavioral;
--- projeto_fish.srcs/sources_1/new/MaquinaEstados_Coef.vhd
+++ projeto_fish.srcs/sources_1/new/MaquinaEstados_Coef.vhd
@@ -4,15 +4,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity MaquinaEstados_Coef is
     Port ( Enter : in STD_LOGIC;
-           Estado : out STD_LOGIC_VECTOR (3 downto 0));
+           Grau : in STD_LOGIC_VECTOR (1 downto 0);
+           Estado : out STD_LOGIC_VECTOR (4 downto 0));
 end MaquinaEstados_Coef;
 
 
 architecture Behavioral of MaquinaEstados_Coef is
-    type Estado_coef is (E0,E1,E2,E3);
+    type Estado_coef is (E0,E1,E2,E3, E4);
     signal Atual_estado, Proximo_estado : Estado_coef;
     
-    signal proximo : STD_LOGIC_VECTOR (3 downto 0);
+    signal proximo : STD_LOGIC_VECTOR (4 downto 0);
 begin
     process(Enter)
     begin
@@ -23,25 +24,50 @@ begin
     
     transicao : process(Atual_estado )
     begin
+    if (Grau = "00") then
+        case Atual_estado is
+            when E0 => Proximo_estado <=E4;
+            when E4 => Proximo_estado <= E0;
+            when others => Proximo_estado <= E0;
+        end case;
+    elsif (Grau = "01") then
+        case Atual_estado is
+            when E0 => Proximo_estado <=E1;       
+            when E1 => Proximo_estado <=E4; 
+            when E4 => Proximo_estado <= E0;
+            when others => Proximo_estado <= E0;
+        end case;
+    elsif (Grau = "10") then
+        case Atual_estado is
+            when E0 => Proximo_estado <=E1;       
+            when E1 => Proximo_estado <=E2; 
+            when E2 => Proximo_estado <=E4; 
+            when E4 => Proximo_estado <= E0;            
+            when others => Proximo_estado <= E0;
+        end case;
+    else
         case Atual_estado is
             when E0 => Proximo_estado <=E1;       
             when E1 => Proximo_estado <=E2; 
             when E2 => Proximo_estado <=E3; 
-            when E3 => Proximo_estado <=E0;
+            when E3 => Proximo_estado <=E4;
+            when E4 => Proximo_estado <= E0;
             when others => Proximo_estado <= E0;
         end case;
+    end if;
     end process;
     
     saida_maquina : process(Atual_estado)
     begin
-        case Atual_estado is
-        when E0 => proximo <="0001";       
-        when E1 => proximo <="0010"; 
-        when E2 => proximo <="0100"; 
-        when E3 => proximo <="1000";
-        when others => proximo <= "0000";
-    end case;        
+    case Atual_estado is
+        when E0 => proximo <="00001";       
+        when E1 => proximo <="00010"; 
+        when E2 => proximo <="00100"; 
+        when E3 => proximo <="01000";
+        when E4 => proximo <="10000";
+        when others => proximo <= "00001";
+    end case;
+
     end process;
-    
     Estado <= proximo;
 end Behavioral;
